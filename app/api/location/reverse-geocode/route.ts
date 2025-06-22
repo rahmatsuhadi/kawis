b27b600d3032d1077abb1c7d296af3595dc0c560
Rahmat <rahmatsuhadi32@gmail.com>
2025-06-22 19:53:46 +0700

    sanbox: testing

--- app/api/location/reverse-geocode/route.ts
+++ app/api/location/reverse-geocode/route.ts
@@ -29,22 +29,23 @@ export async function GET(request: Request) {
     return NextResponse.json({ error: 'Latitude and Longitude are required' }, { status: 400 });
   }
 
- const apiUrl = process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000';
+  const apiUrl = process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000';
   try {
     // Lakukan request ke OpenStreetMap Nominatim API
-    const response = await fetch(
-      `${apiUrl}/api/reverse?lat=${lat}&lon=${lon}&format=json`
-    );
+    // const response = await fetch(
+    //   `${apiUrl}/api/reverse?lat=${lat}&lon=${lon}&format=json`
+    // );
+
+    const response = await fetch("https://nominatim.openstreetmap.org/reverse?format=json&lat=" + lat + "&lon=" + lon)
 
     if (!response.ok) {
-      console.log(await response.json(), response)
-      return NextResponse.json({ error: 'Failed to fetch address from OpenStreetMap' }, { status: 500 });
+      return NextResponse.json({ error: 'Failed to fetch address from OpenStreetMap', response: response }, { status: 500 });
     }
 
     const data: ReverseGeocodeResponse = await response.json();
 
     // Kirimkan alamat sebagai respons
-    return NextResponse.json({ display_name: data.display_name, address: data.address }, { status: 200  });
+    return NextResponse.json({ display_name: data.display_name, address: data.address }, { status: 200 });
   } catch (error) {
     console.log(error)
     // Tangani error
